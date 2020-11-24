950a03670f997a8f096afa695eb1a86ef5eac890
= <=>
2020-11-24 16:23:13 -0300

    fondo y jugador se ven

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -43,49 +43,41 @@ public class Gui extends JFrame {
 	 */
 	public Gui() {
 		
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 		setSize(600,650);
 		setLocationRelativeTo(null);
-		//panelFondo = new ScrollBack();
-		//panelFondo.setLayout(null);
-		//panelFondo.setBounds(0,0, 600,650);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setBounds(0,0,600,630);
 		contentPane.setLayout(null);
 		
-	    ImageIcon icon = new ImageIcon(this.getClass().getResource("/resources/img/3QAe.gif"));
+		iniciarJuego();
+		
+	    ImageIcon icon = new ImageIcon(this.getClass().getResource("/resources/img/fondo.gif"));
 	    JLabel label = new JLabel(icon);
 	    label.setBounds(0,0,600,617);
 	    icon.setImageObserver(label);
-//	    panelFondo = new JPanel();
-//	    panelFondo.setLayout(null);
-//	    panelFondo.setBounds(12,12,500,600);
+	    
 	    contentPane.add(label);
 
 		setContentPane(contentPane);
-	    //contentPane.add(panelFondo);
 		
-//		JPanel panel = new JPanel();
-//		panel.setBounds(517, 0, 171, 617);
-//		contentPane.add(panel);
-//		
-		iniciarJuego();
 	}
 
 	private void iniciarJuego() {
 		juego = new Juego(this);
 		JLabel labelJugador = new JLabel();
-		labelJugador.setOpaque(true);
 		labelJugador.setBounds(300, 549, 31, 42);
 		Entidad jugador = juego.getJugador();
 		jugador.getGrafica().actualizar(0);
 		ImageIcon iconJugador = juego.getJugador().getGrafica().getGrafico();
 		labelJugador.setIcon(iconJugador);
-		iconJugador.setImageObserver(labelJugador);
+		//iconJugador.setImageObserver(labelJugador);
 		contentPane.add(labelJugador);
-		contentPane.validate();
-		contentPane.repaint();
+		this.validate();
+		this.repaint();
 	}
 
 }
