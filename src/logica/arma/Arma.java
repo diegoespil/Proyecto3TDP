3e6384be0b15a058e1bdb13c53806477d987ac9f
bmatus98 <bmatus98@gmail.com>
2020-11-25 18:48:22 -0300

    Arma con patron factory, falta ArmaEnemigo

--- src/logica/arma/Arma.java
+++ src/logica/arma/Arma.java
@@ -5,14 +5,12 @@ import logica.proyectil.Proyectil;
 public abstract class Arma {
 
 	protected int danio;
-	protected Proyectil municion;
 	
 	public Arma(int danio) {
 		this.danio = danio;
-		this.municion = null;
 	}
 	
-	abstract public Proyectil disparar(int x,int y);
+	public abstract Proyectil disparar(int x,int y);
 	
 	public int getDanio() {
 		return danio;
