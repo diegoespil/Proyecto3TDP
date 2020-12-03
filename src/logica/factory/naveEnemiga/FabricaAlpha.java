b9d14062ab1d7e29a9d3d8d5b6e3f07a56eeb954
SilverMigliaro <silve.migliaro@hotmail.com>
2020-12-03 20:07:27 -0300

    nave gamma

--- src/logica/factory/naveEnemiga/FabricaAlpha.java
+++ src/logica/factory/naveEnemiga/FabricaAlpha.java
@@ -1,7 +1,5 @@
 package logica.factory.naveEnemiga;
 
-import java.util.Random;
-
 import logica.naves.Alpha;
 import logica.naves.NaveEnemiga;
 import logica.nivel.Nivel;
@@ -17,26 +15,8 @@ public class FabricaAlpha extends FabricaNaveEnemiga {
 	public NaveEnemiga crearNaveEnemiga() {
 		
 		Alpha nave = new Alpha(0,0);
-		Random r = new Random();
-		int probabilidad = r.nextInt(101); 		
-		if(probabilidad<=30) {
-			if(probabilidad>15) {
-				nave.setPowerUp(this.factoryPocion.crearPremio());
-			}
-			else {
-				if(probabilidad>5) {
-					nave.setPowerUp(this.factoryArma.crearPremio());
-				}
-				else {
-					nave.setPowerUp(this.factoryCuarentena.crearPremio());
-				}
-			}
-		}
-		else {
-			nave.setPowerUp(null);
-		}
-
-		System.out.println("Nave "+nave+" PowerUps "+nave.getPowerUp());
+		nave.setPowerUp(crearPremio());
+		System.out.println(nave+" "+nave.getPowerUp() );
 		return nave;
 	}
 
--- src/logica/factory/naveEnemiga/FabricaNaveEnemiga.java
+++ src/logica/factory/naveEnemiga/FabricaNaveEnemiga.java
@@ -1,10 +1,13 @@
 package logica.factory.naveEnemiga;
 
+import java.util.Random;
+
 import logica.factory.premio.FabricaCuarentena;
 import logica.factory.premio.FabricaPocion;
 import logica.factory.premio.FabricaSuperArma;
 import logica.naves.NaveEnemiga;
 import logica.nivel.Nivel;
+import logica.premios.Premio;
 
 public abstract class FabricaNaveEnemiga {
 
@@ -14,6 +17,7 @@ public abstract class FabricaNaveEnemiga {
 	protected FabricaSuperArma factoryArma;
 	
 	public FabricaNaveEnemiga(Nivel n) {
+		
 		this.nivel = n;
 		factoryCuarentena = new FabricaCuarentena(n);
 		factoryPocion = new FabricaPocion(n);
@@ -21,5 +25,26 @@ public abstract class FabricaNaveEnemiga {
 	}
 	
 	public abstract NaveEnemiga crearNaveEnemiga();
+	
+	protected Premio crearPremio() {
+		Random r = new Random();
+		int probabilidad = r.nextInt(101); 		
+		if(probabilidad<=30) {
+			if(probabilidad>15) {
+				return this.factoryPocion.crearPremio();
+			}
+			else {
+				if(probabilidad>5) {
+					return this.factoryArma.crearPremio();
+				}
+				else {
+					return this.factoryCuarentena.crearPremio();
+				}
+			}
+		}
+		else {
+			 return null;
+		}
+	}
 	
 }
