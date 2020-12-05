9f5017a950e6d6e63de5fa5a4dfaa9d9d76ab578
= <=>
2020-12-04 23:02:08 -0300

    posicion de proyectiles mas centrada

--- src/logica/arma/ArmaJugador.java
+++ src/logica/arma/ArmaJugador.java
@@ -13,8 +13,9 @@ public class ArmaJugador extends Arma{
 	@Override
 	public void disparar(int x,int y) {
 		ProyectilJugador p = new ProyectilJugador(x,y,this.velocidad,this.danio);
+		p.setPosX(x+9);
 		p.setPosY(y-p.getGrafica().getHeight());
-		p.getGrafica().setBounds(x,y, p.getGrafica().getWidth(), p.getGrafica().getHeight());
+		p.getGrafica().setBounds(p.getPosX(),p.getPosY(), p.getGrafica().getWidth(), p.getGrafica().getHeight());
 		Juego.getInstance().agregarEntidad(p);
 		Gui.getInstance().agregarEntidad(p.getGrafica().getLabel());
 	}
--- src/logica/arma/ArmaSuper.java
+++ src/logica/arma/ArmaSuper.java
@@ -13,8 +13,9 @@ public class ArmaSuper extends ArmaJugador {
 	public void disparar(int x,int y) {
 		ProyectilJugador p = new ProyectilJugador(x,y,this.velocidad,this.danio);
 		p.getGrafica().actualizar(1);
+		//p.setPosX(x+1);
 		p.setPosY(y-p.getGrafica().getHeight());
-		p.getGrafica().setBounds(x,y, p.getGrafica().getWidth(), p.getGrafica().getHeight());
+		p.getGrafica().setBounds(p.getPosX(),p.getPosY(), p.getGrafica().getWidth(), p.getGrafica().getHeight());
 		Juego.getInstance().agregarEntidad(p);
 		Gui.getInstance().agregarEntidad(p.getGrafica().getLabel());
 	}
