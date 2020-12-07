624b6cf7735dfafdca33183562a50423cb096d1e
SilverMigliaro <silve.migliaro@hotmail.com>
2020-12-07 19:01:45 -0300

    arregle el disparo jugador

--- src/logica/arma/ArmaJugador.java
+++ src/logica/arma/ArmaJugador.java
@@ -13,7 +13,7 @@ public class ArmaJugador extends Arma{
 	@Override
 	public void disparar(int x,int y) {
 		ProyectilJugador p = new ProyectilJugador(x,y,this.velocidad,this.danio);
-		p.setPosX(x+9);
+		//p.setPosX(x+9);
 		p.setPosY(y-p.getGrafica().getHeight());
 		p.getGrafica().setBounds(p.getPosX(),p.getPosY(), p.getGrafica().getWidth(), p.getGrafica().getHeight());
 		Juego.getInstance().agregarEntidad(p);
